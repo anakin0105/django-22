4081188b91e242f2cd1ff0a92b197a502fef802f
Анна Акиньшина <anakin0105@gmail.com>
2026-03-13 18:57:36 +0300

    Сделаны страный и проект

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from catalog.apps import NewappConfig
+from catalog.views import home
+from . import views
+
+
+app_name = NewappConfig.name
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+]
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-$m-zjb)e@z+o@ai*m^83s=jo_&q%jw@a!(j4)+sxlp*x#-l@7^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -115,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
